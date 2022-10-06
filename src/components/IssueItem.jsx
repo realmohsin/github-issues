5af4537d420308943ce8839521162847d8bf1706
Real Mohsin <real@realmohsin.com>
2022-10-05 23:21:46 -0400

    fetching issues, then fetching users in each IssueItem

---
+++ src/components/IssueItem.jsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom";
+import { GoIssueOpened, GoIssueClosed, GoComment } from "react-icons/go";
+import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
+
+export function IssueItem({
+  key,
+  title,
+  number,
+  assignee,
+  commentCount,
+  createdBy,
+  createdDate,
+  labels,
+  status,
+}) {
+  const assigneeUser = useUserData(assignee);
+  const createdByUser = useUserData(createdBy);
+  return (
+    <li>
+      <div>
+        {status === "done" || status === "cancelled" ? (
+          <GoIssueClosed style={{ color: "red" }} />
+        ) : (
+          <GoIssueOpened style={{ color: "green" }} />
+        )}
+      </div>
+      <div className="issue-content">
+        <span>
+          <Link to={`/issue/${number}`}>{title}</Link>
+          {labels.map((label) => (
+            <span key={label} className={`label red`}>
+              {label}
+            </span>
+          ))}
+        </span>
+        <small>
+          #{number} opened {relativeDate(createdDate)}{" "}
+          {createdByUser.isSuccess ? `by ${createdByUser.data.name}` : ""}
+        </small>
+      </div>
+      {assignee ? (
+        <img
+          src={
+            assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ""
+          }
+          className="assigned-to"
+          alt={`Assigned to ${
+            assigneeUser.isSuccess ? assigneeUser.data.name : ""
+          }`}
+        />
+      ) : null}
+      <span className="comment-count">
+        {commentCount > 0 ? (
+          <>
+            <GoComment />
+            {commentCount}
+          </>
+        ) : null}
+      </span>
+    </li>
+  );
+}
